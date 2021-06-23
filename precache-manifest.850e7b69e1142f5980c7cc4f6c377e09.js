4643223f23a1723c0ba5df28cdddc6df60f3457d
Tariq <tariq@sabaseo.com>
2021-06-22 21:26:01 -0700

    Updates

---
+++ precache-manifest.850e7b69e1142f5980c7cc4f6c377e09.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f319c4e4d1074e005ad4275d32ee05bb",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "aaa02d4b1bfb600e41c9",
+    "url": "/my-react-portfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5ae94434a16c937ff2e8",
+    "url": "/my-react-portfolio/static/css/main.4d435614.chunk.css"
+  },
+  {
+    "revision": "aaa02d4b1bfb600e41c9",
+    "url": "/my-react-portfolio/static/js/2.0815af98.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/my-react-portfolio/static/js/2.0815af98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae94434a16c937ff2e8",
+    "url": "/my-react-portfolio/static/js/main.f37cccf3.chunk.js"
+  },
+  {
+    "revision": "da9aa0969b58fb8e5ed5",
+    "url": "/my-react-portfolio/static/js/runtime-main.23411ad0.js"
+  },
+  {
+    "revision": "ab2fb76bebc202f71d0f1720782afc6b",
+    "url": "/my-react-portfolio/static/media/CECOS-University-Pk.ab2fb76b.png"
+  },
+  {
+    "revision": "13e48a454cb12ed1c018d3acd1459075",
+    "url": "/my-react-portfolio/static/media/Home-Care-Assistance.13e48a45.png"
+  },
+  {
+    "revision": "a3323cced476580329e3bfda60e998b6",
+    "url": "/my-react-portfolio/static/media/Javascript-regex.a3323cce.png"
+  },
+  {
+    "revision": "38dde8eae0ab05d334c2d9bfc2b283b9",
+    "url": "/my-react-portfolio/static/media/LA-Spine.38dde8ea.png"
+  },
+  {
+    "revision": "f1608260ac6895bf33e922e91c431969",
+    "url": "/my-react-portfolio/static/media/LIver-Center.f1608260.png"
+  },
+  {
+    "revision": "0227d318f321acc29bf8bd51bf80b80f",
+    "url": "/my-react-portfolio/static/media/ORM-Ecommerce-Backend.0227d318.png"
+  },
+  {
+    "revision": "4f3a190752496727c68f52ee1a14d311",
+    "url": "/my-react-portfolio/static/media/SocialNetworkApi.4f3a1907.png"
+  },
+  {
+    "revision": "740183e966a5e1ec306d893dd8219237",
+    "url": "/my-react-portfolio/static/media/Workday-Schduler.740183e9.png"
+  },
+  {
+    "revision": "41dd8e27834adbca3c39e5cfb23882fb",
+    "url": "/my-react-portfolio/static/media/avatar.41dd8e27.png"
+  },
+  {
+    "revision": "1dcf6b68cc8f07eef99834c877f9abd0",
+    "url": "/my-react-portfolio/static/media/berkeley.1dcf6b68.png"
+  },
+  {
+    "revision": "4bb62f3c2a6c2b09fc75d23eaa74fcfb",
+    "url": "/my-react-portfolio/static/media/bootstrap-logo.4bb62f3c.png"
+  },
+  {
+    "revision": "379fe880d7638d8d5e6493d9dcfac94c",
+    "url": "/my-react-portfolio/static/media/jquery-logo.379fe880.png"
+  },
+  {
+    "revision": "4da35bbb28b8689e03167acf8e460cdc",
+    "url": "/my-react-portfolio/static/media/left-arrow.4da35bbb.svg"
+  },
+  {
+    "revision": "86695d5d2f75c08615e5db9c648e099f",
+    "url": "/my-react-portfolio/static/media/logo.86695d5d.png"
+  },
+  {
+    "revision": "8967ca67bbedb067ba0aedd74a407295",
+    "url": "/my-react-portfolio/static/media/mern-logo.8967ca67.png"
+  },
+  {
+    "revision": "541dcec40560667c49cccd764392f3d4",
+    "url": "/my-react-portfolio/static/media/mongodb-logo.541dcec4.png"
+  },
+  {
+    "revision": "fde28721637447d8cc5f9eb73f546e95",
+    "url": "/my-react-portfolio/static/media/note-taker.fde28721.png"
+  },
+  {
+    "revision": "f1f167c6e0ec3093c0b2ed0cc07b1024",
+    "url": "/my-react-portfolio/static/media/pwa-budget-tracker.f1f167c6.png"
+  },
+  {
+    "revision": "19080b3cc1962fff3d289ffd360e640d",
+    "url": "/my-react-portfolio/static/media/redux-logo.19080b3c.png"
+  },
+  {
+    "revision": "64c224f4d2f224353b11f05ad1822c71",
+    "url": "/my-react-portfolio/static/media/right-arrow.64c224f4.svg"
+  },
+  {
+    "revision": "56ea5915b022ad2916379c326bfc9302",
+    "url": "/my-react-portfolio/static/media/sql-employee-tracker.56ea5915.png"
+  },
+  {
+    "revision": "21812f6d8d4844da17453b80216a4445",
+    "url": "/my-react-portfolio/static/media/sql-logo.21812f6d.png"
+  },
+  {
+    "revision": "107e080e3a343145f9d01a5a33d29b60",
+    "url": "/my-react-portfolio/static/media/tech-blog.107e080e.png"
+  },
+  {
+    "revision": "08f0c81fc056ac68a91d127aa3a13657",
+    "url": "/my-react-portfolio/static/media/vuejs-logo.08f0c81f.png"
+  },
+  {
+    "revision": "db21fd0a82bcf0de842e63b76d7ff841",
+    "url": "/my-react-portfolio/static/media/weather-dashboard.db21fd0a.png"
+  },
+  {
+    "revision": "f7099627bd7c464de1c0b40abf87ad86",
+    "url": "/my-react-portfolio/static/media/webpack-logo.f7099627.png"
+  }
+]);
